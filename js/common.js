adfdf93f6a1d262f7e8f214bd669e5809d4330a4
Shmiklak <shmiklak@gmail.com>
2020-09-19 18:56:30 +0500

    initial

---
+++ js/common.js
@@ -0,0 +1,25 @@
+$(function() {
+
+    new WOW().init();
+
+	$(".mobile-nav-toggle").on('click', function() {
+	   $('.header-nav-list').addClass('active');
+	   $('body').addClass('fade');
+    });
+
+
+    $(window).click(function() {
+        $('.header-nav-list').removeClass('active');
+        $('body').removeClass('fade');
+    });
+
+    $('.header-nav-list, .mobile-nav-toggle').click(function(event){
+        event.stopPropagation();
+    });
+
+    Splitting();
+
+    $(window).on('load', function () {
+        $('[data-splitting], .banner').addClass('loaded');
+    });
+});
